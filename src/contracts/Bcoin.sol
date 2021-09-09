44860762e0484be5fe4ed8cd97a167813e648867
Boluwatife Ayodele <waplurd1@gmail.com>
2021-09-09 02:15:11 +0100

    created profile for coin

---
+++ src/contracts/Bcoin.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+contract Bcoin {
+    string public name = "Bcoin";
+    string public symbol = "USDT";
+    uint public totalSupply = 1000000000000000000000000;
+    uint8 public decimals = 18;
+}
