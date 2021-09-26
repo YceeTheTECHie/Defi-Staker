23d7446af9c57fcbeb0a45fc1e0ab6ddde31d824
Boluwatife Ayodele <waplurd1@gmail.com>
2021-09-26 01:31:07 +0100

    addes test for transfer operations

--- test/decentralizedBank.tests.js
+++ test/decentralizedBank.tests.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+const { default: Web3 } = require('web3');
 const Bcoin = artifacts.require('Bcoin');
 const Rwd = artifacts.require('Rwd');
 const DecentralizedBank = artifacts.require('DecentralizedBank');
@@ -8,9 +9,18 @@ require("chai")
     .should();
 
 contract(DecentralizedBank, (accounts) => {
-    let bcoin,Rwd
+
+    // helper function to help token formatting
+    const tokenFormatter = amount => {
+        return Web3.utils.toWei(amount, 'tether')
+    }
+    let bcoin,rwd
     before(async () => {
         bcoin = await Bcoin.new();
+        rwd = await Rwd.new();
+        decentralizedBank = DecentralizedBank.new(rwd.address, bcoin.address);
+        await rwd.transfer(decentralizedBank.address, tokenFormatter(1000000));
+        await bcoin.transfer(accounts[1],tokenFormatter())
     })
     describe('Token Deployment', async () => {
         it('name matches', async () => {
